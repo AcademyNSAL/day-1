2f4d8f038d35910bbe6d9419db14821a0aecdee2
Muhammad Hudair <aliahmad3434434@gmail.com>
2024-09-05 18:59:56 +0500

    assignment 1

---
+++ assignment.c
@@ -0,0 +1,5 @@
+// print you name in UPPERCASE and lowercase
+// print you name first latter in scanf
+// Example code no 2.6
+// https://www.programiz.com/c-programming/online-compiler/
+// https://forms.gle/9NpssxKaGW8hGdCD9
