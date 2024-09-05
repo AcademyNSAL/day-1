eaebe311b7d90b487c974d941fe605712994d76c
Muhammad Hudair <aliahmad3434434@gmail.com>
2024-09-05 18:40:43 +0500

    activity

---
+++ activity.c
@@ -0,0 +1,18 @@
+// print you name in UPPERCASE and lowercase
+// print you name first latter in scanf
+
+// #include<stdio.h>
+
+// void main(){
+//     printf("MOAZ bin amir");
+// }
+
+// #include <stdio.h>
+
+// void main()
+// {
+//     char name; // variable declare with the name of "name".
+//     printf("Enter your name first character: "); // this is for showing for getting the user name first character.
+//     scanf("%c", &name); // this for getting name first character
+//     printf("your name first character is: %c", name); // this is for print final result
+// }
