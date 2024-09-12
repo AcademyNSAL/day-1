020f7c1c15bad990499c12a5585d99a52ffaace7
Muhammad Hudair <aliahmad3434434@gmail.com>
2024-09-12 18:39:01 +0500

    Assignment 2

--- assignment.c
+++ assignment.c
@@ -3,3 +3,6 @@
 // Example code no 2.6
 // https://www.programiz.com/c-programming/online-compiler/
 // https://forms.gle/9NpssxKaGW8hGdCD9
+
+// Assignment 2
+// Write a program thar takes 2 digit as input from user, computes the product of both digits and show the output
